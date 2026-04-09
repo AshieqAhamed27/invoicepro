580c104fe81435f647ee21cde064c7c3b7b9c5a9
AshieqAhamed27 <ashieqahamed4@gmail.com>
2026-04-10 00:11:02 +0530

    fix homepage routing + add landing page

--- invoicepro/frontend/src/App.jsx
+++ invoicepro/frontend/src/App.jsx
@@ -65,12 +65,55 @@ export default function App() {
           </PrivateRoute>
         } />
 
-          <Route path="/admin" element={<Admin />} />
+        <Route path="/admin" element={<Admin />} />
 
-          <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
+
+      </Routes><Routes>
+
+        {/* HOMEPAGE */}
+        <Route path="/" element={<Home />} />
+
+        <Route path="/login" element={
+          <PublicRoute>
+            <Login />
+          </PublicRoute>
+        } />
+
+        <Route path="/signup" element={
+          <PublicRoute>
+            <Signup />
+          </PublicRoute>
+        } />
+
+        <Route path="/dashboard" element={
+          <PrivateRoute>
+            <Dashboard />
+          </PrivateRoute>
+        } />
+
+        <Route path="/create-invoice" element={
+          <PrivateRoute>
+            <CreateInvoice />
+          </PrivateRoute>
+        } />
+
+        <Route path="/invoice/:id" element={
+          <PrivateRoute>
+            <InvoiceView />
+          </PrivateRoute>
+        } />
+
+        <Route path="/payment" element={
+          <PrivateRoute>
+            <Payment />
+          </PrivateRoute>
+        } />
+
+        <Route path="/admin" element={<Admin />} />
 
       </Routes>
-    
+
     </BrowserRouter>
   );
 }
