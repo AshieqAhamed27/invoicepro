1994ad8e3313f24269bf57ceb014a092c5994bd7
AshieqAhamed27 <ashieqahamed4@gmail.com>
2026-04-21 12:03:59 +0530

    fix error

--- invoicepro/frontend/src/pages/Admin.jsx
+++ invoicepro/frontend/src/pages/Admin.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import api, { API_BASE_URL, API_ORIGIN } from '../utils/api';
+import api from '../utils/api';
 import Navbar from '../components/Navbar';
 
 export default function Admin() {
